e04dd03d8d56720bb17dd04ba455bdf57dad2f9e
Emmanouil Zacharakis <mn.zacharakis@gmail.com>
2026-03-10 15:22:56 +0100

    Forward declarations of gtk types

--- src/webview.h
+++ src/webview.h
@@ -1,6 +1,16 @@
 #ifndef WEBVIEW_H
 #define WEBVIEW_H
 
+/*
+ * Use forward declarations to make header file self-contained and suppress
+ * clang warngings and errors. Since the struct only holds pointers to those
+ * types it will work. gchar still needs to be included from glib.h.
+ */
+#include <glib.h>
+typedef struct _GtkBox GtkBox;
+typedef struct _GtkWidget GtkWidget;
+typedef struct _WebKitWebView WebKitWebView;
+
 typedef struct  {
 	GtkBox *box;
 	GtkWidget *bar;
