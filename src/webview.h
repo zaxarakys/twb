cb16866efcbfac1c45ebf2efb5b1041a244e20dc
Emmanouil Zacharakis <mn.zacharakis@gmail.com>
2026-03-10 15:19:25 +0100

    Made twb_web_view struct

--- src/webview.h
+++ src/webview.h
@@ -1,6 +1,12 @@
 #ifndef WEBVIEW_H
 #define WEBVIEW_H
 
+typedef struct  {
+	GtkBox *box;
+	GtkWidget *bar;
+	WebKitWebView *webview;
+} twb_web_view; 
+
 WebKitWebView *create_web_view(const gchar *url);
 
 #endif /* WEBVIEW_H */
