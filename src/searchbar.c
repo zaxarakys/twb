eb379ee29f7b843d8ebfdc5b93544f6b18543e0e
Emmanouil Zacharakis <mn.zacharakis@gmail.com>
2026-03-10 23:35:49 +0100

    Unfocus url bar on page load

--- src/searchbar.c
+++ src/searchbar.c
@@ -10,6 +10,7 @@ static void search_bar_enter (GtkEntry *search_bar, gpointer user_data) {
 
 	twb_web_view *twv = twb_web_view_new(url);
 	gtk_overlay_add_overlay(GTK_OVERLAY(overlay), GTK_WIDGET(twv->box));
+	gtk_widget_grab_focus(GTK_WIDGET(twv->webview));
 }
 
 
